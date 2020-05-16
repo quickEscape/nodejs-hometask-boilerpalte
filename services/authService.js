28c4c181fcf44b3e75e5efc5269ebba1b3480e6b
Yevhenii Kononko <yevhenii.kononko@playtika.com>
2020-05-16 22:17:27 +0300

    Update auth service

--- services/authService.js
+++ services/authService.js
@@ -1,13 +1,13 @@
-const UserService = require('./userService');
+const { UserService } = require("./userService");
 
 class AuthService {
-    login(userData) {
-        const user = UserService.search(userData);
-        if(!user) {
-            throw Error('User not found');
-        }
-        return user;
+  login(userData) {
+    const user = UserService.search(userData);
+    if (!user) {
+      throw Error("User not found");
     }
+    return user;
+  }
 }
 
 module.exports = new AuthService();
