df616e50e8523f2bb7157f76b88b05611e4e705a
Yevhenii Kononko <yevhenii.kononko@playtika.com>
2020-05-16 22:14:08 +0300

    Add fighter service

--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,46 @@
-const { FighterRepository } = require('../repositories/fighterRepository');
+const { FighterRepository } = require("../repositories/fighterRepository");
 
 class FighterService {
-    // TODO: Implement methods to work with fighters
+  // TODO: Implement methods to work with fighters
+  getFighters() {
+    const fighters = FighterRepository.getAll();
+
+    if (fighters.length) return fighters;
+
+    return null;
+  }
+
+  getFighter(id) {
+    const fighter = FighterRepository.getOne(id);
+
+    if (fighter) return fighter;
+
+    return null;
+  }
+
+  createFighter(data) {
+    const fighter = FighterRepository.create(data);
+
+    if (fighter) return fighter;
+
+    return null;
+  }
+
+  updateFighter(id, data) {
+    const fighter = FighterRepository.update(id, data);
+
+    if (fighter) return fighter;
+
+    return null;
+  }
+
+  deleteFighter(id) {
+    const fighter = FighterRepository.delete(id);
+
+    if (fighter) return fighter;
+
+    return null;
+  }
 }
 
 module.exports = new FighterService();
