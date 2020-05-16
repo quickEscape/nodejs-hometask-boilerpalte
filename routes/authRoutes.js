0a0b49dbab0dbdd603d149dc0e716124b8f42cf4
Yevhenii Kononko <yevhenii.kononko@playtika.com>
2020-05-16 22:16:57 +0300

    Add auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,28 @@
-const { Router } = require('express');
-const AuthService = require('../services/authService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const AuthService = require("../services/authService");
+const { responseMiddleware } = require("../middlewares/response.middleware");
+const { error } = require("../services/service");
+const { clearUser } = require("../services/userService");
 
 const router = Router();
 
-router.post('/login', (req, res, next) => {
+router.post(
+  "/login",
+  (req, res, next) => {
     try {
-        // TODO: Implement login action (get the user if it exist with entered credentials)
-        res.data = data;
+      // TODO: Implement login action (get the user if it exist with entered credentials)
+      const user = AuthService.login({
+        email: req.body.email,
+        password: req.body.password
+      });
+      res.status(200).json({ meta: "Login successful", data: clearUser(user) });
     } catch (err) {
-        res.err = err;
+      res.status(403).json(error(err));
     } finally {
-        next();
+      next();
     }
-}, responseMiddleware);
+  },
+  responseMiddleware
+);
 
 module.exports = router;
