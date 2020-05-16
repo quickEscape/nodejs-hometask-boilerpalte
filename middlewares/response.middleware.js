876d2035686357e7e482c4c9205bd4712f17f9c8
Yevhenii Kononko <yevhenii.kononko@playtika.com>
2020-05-16 23:21:42 +0300

    Add comment

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,6 +1,8 @@
 const responseMiddleware = (req, res, next) => {
-   // TODO: Implement middleware that returns result of the query
-    next();
-}
+  // TODO: Implement middleware that returns result of the query
+  // запутался с этим мидлваром :(
+  // вроде как вместо того, чтобы делать одно и то же в роутингах нужно было сделать это тут
+  next();
+};
 
 exports.responseMiddleware = responseMiddleware;
